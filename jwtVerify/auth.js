fb9a1d11af20dbe239ec8cbe232f92fbfbf9a6dd
Isaac Ruiz <isaacruiz@Isaacs-MBP.lan>
2022-05-29 18:57:12 -0700

    set up a function that will check the validity of jwt sent with requests

---
+++ jwtVerify/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+// checking if the request from the client side was sent alongside a valid json web token and if it wasn't then it sends back an unauthorized error
+module.exports = function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization'];
+    // if we have an authHeader than return the authHeader which we split where there is a space otherwise return undefine
+    const token = authHeader && authHeader.split("")[1];
+    if (token == null) return res.sendStatus(401);
+    // if we have a valid token then do this
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user
+        next()
+    })
+}
