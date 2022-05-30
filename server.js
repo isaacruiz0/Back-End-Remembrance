2dd125326b798aefdcc63599ead3a399fc69a895
Isaac Ruiz <isaacruiz@Isaacs-MBP.lan>
2022-05-29 18:47:41 -0700

    implemented passport authentication in the server.js file

--- server.js
+++ server.js
@@ -14,8 +14,29 @@ const PORT = process.env.PORT
 const app = express()
 
 const User = require('./models/User')
+
+// Express Session Middleware
+app.use(require('express-session')({
+    secret: "Nothing",
+    resave: false,
+    saveUninitialized: false
+}));
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next()
+})
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
+app.use(bodyParser.urlencoded({extended:false}))
 // Middleware that allows json to be accepted
-app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
